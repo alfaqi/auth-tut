94d4ccc531be1e709481d24cea215f2b4897318e
alfaqi <alfaqiabdullah@gmail.com>
2025-09-26 00:39:09 +0300

    Update the port number

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello");
