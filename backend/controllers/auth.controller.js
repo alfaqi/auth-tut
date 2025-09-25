5e8dca6b4ec220b6539f50e745e4314d05023e3c
alfaqi <alfaqiabdullah@gmail.com>
2025-09-26 00:39:57 +0300

    Add 'auth.controller.js' file

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("Signup route controller");
+};
+
+export const login = async (req, res) => {
+  res.send("Login route controller");
+};
+
+export const logout = async (req, res) => {
+  res.send("Logout route controller");
+};
