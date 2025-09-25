b7331bddb11a1e1100963186fa70eb85d082fad2
alfaqi <alfaqiabdullah@gmail.com>
2025-09-26 00:28:38 +0300

    Add 'index.js' file

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
