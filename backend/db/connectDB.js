7a3cc2a0c39d7f870f49f264d3da05a13138626a
alfaqi <alfaqiabdullah@gmail.com>
2025-09-26 00:28:17 +0300

    Add 'connectDB.js' file

---
+++ backend/db/connectDB.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to database:", connection.connection.host);
+    console.log("Database connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
