4187bebc5276b29bebfac7adb0dbd417562e4d5f
alfaqi <alfaqiabdullah@gmail.com>
2025-09-26 20:17:57 +0300

    Update 'signup' controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,61 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import {
+  generateExpiryDate,
+  generateTokenAndSetCookie,
+  generateVerificationCode,
+} from "../utils/utils.js";
+import { sendVerificationEmail } from "../mailtrap/emails.js";
+
 export const signup = async (req, res) => {
-  res.send("Signup route controller");
+  try {
+    const { name, email, password } = req.body;
+    console.log(req.body);
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const verificationCode = generateVerificationCode();
+    const verificationCodeExpiresAt = generateExpiryDate(60);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      verificationCode,
+      verificationCodeExpiresAt,
+    });
+
+    console.log(user);
+
+    const token = generateTokenAndSetCookie(res, user._id);
+
+    await sendVerificationEmail(user.email, verificationCode);
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: {
+        user: {
+          ...user._doc,
+          password: undefined,
+        },
+      },
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 export const login = async (req, res) => {
