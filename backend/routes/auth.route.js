f5aa8bd5f0f336bc57d5484d2fa4a73c40e8ed77
alfaqi <alfaqiabdullah@gmail.com>
2025-09-28 00:03:34 +0300

    Clean up

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -16,16 +16,12 @@ const router = express.Router();
 router.get("/check-auth", verifyToken, checkAuth);
 
 router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 router.post("/verify-email", verifyEmail);
-
 router.post("/forgot-password", forgotPassword);
-
 router.post("/reset-password/:token", resetPassword);
-
-router.post("/login", login);
-
-router.post("/logout", logout);
 
 router.post("/welcome", welcome);
 
