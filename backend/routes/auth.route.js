669a575d90a0e2dcd5acc7eb03f744061d45f5d0
alfaqi <alfaqiabdullah@gmail.com>
2025-09-27 18:06:41 +0300

    Add 'verify-email' endpoint

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  signup,
+  verifyEmail,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+
+router.post("/verify-email", verifyEmail);
 
 router.post("/login", login);
 
