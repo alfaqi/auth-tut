f5805a5db3e61e22a380851f26b8d99eeab95174
alfaqi <alfaqiabdullah@gmail.com>
2025-09-27 18:47:25 +0300

    Update 'userSchema' schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    lastDate: { type: Date, default: Date.now },
+    lastLogin: { type: Date, default: Date.now },
     isVerified: { type: Boolean, default: false },
     resetPasswordCode: { type: String, default: null },
     resetPasswordExpiresAt: { type: Date, default: null },
