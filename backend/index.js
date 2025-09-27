1e0ffd91fe1bebd99bfaea14aaa7c43a26e2dde6
alfaqi <alfaqiabdullah@gmail.com>
2025-09-28 00:04:37 +0300

    Accepting the cookies

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 
@@ -9,6 +11,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse json data from JSON payloads
+app.use(cookieParser()); // allows us to paser incoming cookies
 
 app.use("/api/auth", authRoutes);
 
