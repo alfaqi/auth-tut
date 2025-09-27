b0b29738eab7899129753ca3d2b817930ccbc14a
alfaqi <alfaqiabdullah@gmail.com>
2025-09-27 18:29:43 +0300

    Update 'logout' controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -107,5 +107,6 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  res.send("Logout route controller");
+  res.clearCookie("token");
+  res.status(200).json({ success: true, message: "Logged out successfully" });
 };
