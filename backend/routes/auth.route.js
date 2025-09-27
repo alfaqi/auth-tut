32e4bad4ec33603e75f29d7cc1f8cd06e3cae716
alfaqi <alfaqiabdullah@gmail.com>
2025-09-28 00:03:23 +0300

    Add 'check-auth' endpoint

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  checkAuth,
   login,
   logout,
   signup,
@@ -8,8 +9,11 @@ import {
   resetPassword,
   welcome,
 } from "../controllers/auth.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/check-auth", verifyToken, checkAuth);
 
 router.post("/signup", signup);
 
