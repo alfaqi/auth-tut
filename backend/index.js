1b927db090bf766b3103dcca6057bc8ef95858c6
alfaqi <alfaqiabdullah@gmail.com>
2025-09-30 01:06:33 +0300

    Minor update

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,10 @@ const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 
 // middlewares
-app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: CLIENT_URL, credentials: true }));
 app.use(express.json()); // to parse json data from JSON payloads
 app.use(cookieParser()); // allows us to paser incoming cookies
 
